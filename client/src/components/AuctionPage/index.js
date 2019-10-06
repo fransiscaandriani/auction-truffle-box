5a69ecddff4b89728df5d0bf6f787f51068e2fde
Fransisca Andriani <fran0025@e.ntu.edu.sg>
2019-10-07 06:41:56 +0800

    Fix formatting in Auctionpage
    Signed-off-by: Fransisca Andriani <fran0025@e.ntu.edu.sg>

--- client/src/components/AuctionPage/index.js
+++ client/src/components/AuctionPage/index.js
@@ -1,69 +1,69 @@
-import React, { useEffect, useState, useCallback } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Container, Typography, Button, TextField } from '@material-ui/core';
-import { placeBid } from '../../service/bidderService';
-import { getLoadedWeb3 } from '../../utils/getWeb3';
-import getCurrentAccount from '../../utils/getCurrentAccount';
-import { getAuctionContract } from '../../utils/getContracts';
-import debounce from 'lodash/debounce';
+import React, { useEffect, useState, useCallback } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Container, Typography, Button, TextField } from "@material-ui/core";
+import { placeBid } from "../../service/bidderService";
+import { getLoadedWeb3 } from "../../utils/getWeb3";
+import getCurrentAccount from "../../utils/getCurrentAccount";
+import { getAuctionContract } from "../../utils/getContracts";
+import debounce from "lodash/debounce";
 
 const useStyles = makeStyles({
   title: {
-    marginTop: 40,
+    marginTop: 40
   },
   image: {
     width: 600,
     margin: 20,
     marginLeft: 0,
     height: 300,
-    objectFit: 'cover',
+    objectFit: "cover"
   },
   description: {
-    maxWidth: 600,
+    maxWidth: 600
   },
   action: {
-    display: 'flex',
+    display: "flex"
   },
-  'button-container': {
-    display: 'flex',
-    flexDirection: 'column',
-    height: 50,
+  "button-container": {
+    display: "flex",
+    flexDirection: "column",
+    height: 50
   },
   input: {
-    margin: 10,
+    margin: 10
   },
   end: {
-    margin: 20,
-  },
+    margin: 20
+  }
 });
 
 function AuctionPage() {
   const classes = useStyles();
   const [bid, setBid] = useState(0);
-  const [account, setAccount] = useState('');
+  const [account, setAccount] = useState("");
   const [auctionContract, setAuctionContract] = useState({});
   const [web3, setWeb3] = useState({});
-  const [cipher, setCipher] = useState('');
+  const [cipher, setCipher] = useState("");
 
   useEffect(() => {
     async function fetchData() {
-      console.log('inside fetch data');
+      console.log("inside fetch data");
       try {
         // Get network provider and web3 instance.
-        console.log('inside try');
+        console.log("inside try");
         const web3 = await getLoadedWeb3();
-        console.log('web3', web3);
+        console.log("web3", web3);
         setWeb3(web3);
 
         const auctionContract = await getAuctionContract(
           web3,
-          '0x5e19053f26BC45c38C6e5612aDf6025b5e3D768f'
+          "0x13c86Eb528226346E523a5fC8aD7fe299360bf0d"
         );
         setAuctionContract(auctionContract);
-        console.log('auction cotnract: ', auctionContract);
+        console.log("auction cotnract: ", auctionContract);
 
         const account = await getCurrentAccount(web3);
-        console.log('account: ', account);
+        console.log("account: ", account);
         setAccount(account);
       } catch (error) {
         // Catch any errors for any of the above operations.
@@ -74,7 +74,7 @@ function AuctionPage() {
       }
     }
 
-    console.log('inside usestate');
+    console.log("inside usestate");
     fetchData();
   }, []);
 
@@ -88,13 +88,13 @@ function AuctionPage() {
     setCipher(cipher);
   };
 
-  console.log('account:', account);
+  console.log("account:", account);
 
-  console.log('bid', bid);
+  console.log("bid", bid);
 
   const renderCipher = () => {
-    console.log('cipher', cipher);
-    if (cipher !== '') {
+    console.log("cipher", cipher);
+    if (cipher !== "") {
       return (
         <Typography className={classes.end} variant="h5">
           Cipher: {cipher}
@@ -114,7 +114,7 @@ function AuctionPage() {
           src="https://pbs.twimg.com/media/DVHZv6cWsAIr4at.jpg:large"
           alt="auction-item"
         />
-        <div className={classes['button-container']}>
+        <div className={classes["button-container"]}>
           <Typography className={classes.end} variant="h5">
             Auction closes in 12:12:22
           </Typography>
@@ -125,7 +125,7 @@ function AuctionPage() {
             type="number"
             className={classes.textField}
             InputLabelProps={{
-              shrink: true,
+              shrink: true
             }}
             defaultValue={1}
             margin="normal"

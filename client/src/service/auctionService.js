2189ccf215a924685c95b1214e72e6e54daa55ff
Fransisca Andriani <fran0025@e.ntu.edu.sg>
2019-10-07 06:43:05 +0800

    Add function to get all auctions data. Fix generating public key for auctioneer based on passphrase
    Signed-off-by: Fransisca Andriani <fran0025@e.ntu.edu.sg>

--- client/src/service/auctionService.js
+++ client/src/service/auctionService.js
@@ -1,22 +1,26 @@
+import Cryptico from "cryptico-js";
+
 const mockAuction = {
-  name: "Mock Auction",
+  name: "Mock Auction with correct public key",
   desc: "Auction for testing purposes",
   bidEndTime: 1570406400,
   revealTime: 1570416400,
   winnerPaymentTime: 1570426400,
   maxBiddersCount: 20,
   fairnessFees: 5000000000,
-  auctioneerRSAPublicKey: "abcde",
+  passphrase: "abcdefgh",
   k: 10,
   testing: true
 };
 
-export function createAuction(
+export async function createAuction(
   account,
   web3,
   contract,
   auctionData = mockAuction
 ) {
+  const rsaKey = Cryptico.generateRSAKey(auctionData.passphrase, 1024);
+  const publicKey = Cryptico.publicKeyString(rsaKey);
   contract.methods
     .createAuction(
       auctionData.name,
@@ -26,7 +30,7 @@ export function createAuction(
       auctionData.winnerPaymentTime,
       auctionData.maxBiddersCount,
       auctionData.fairnessFees,
-      auctionData.auctioneerRSAPublicKey,
+      publicKey,
       auctionData.k,
       auctionData.testing
     )
@@ -34,4 +38,16 @@ export function createAuction(
       from: account,
       value: web3.utils.toWei(auctionData.fairnessFees.toString(), "wei")
     });
+}
+
+export async function getAllAuctionsData(web3, contract) {
+  var auctionsData = [];
+  const auctionsAddresses = await contract.methods.allAuctions().call();
+  auctionsAddresses.forEach(async function(address) {
+    const temp = await contract.methods.getAuctionData(address).call();
+    const data = { address: address, name: temp[0], desc: temp[1] };
+    auctionsData.push(data);
+  });
+
+  return auctionsData;
 }

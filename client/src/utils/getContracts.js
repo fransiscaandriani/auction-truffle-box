b016c064dd221d4efb15f9aee194e4c49701cc02
Fransisca Andriani <fran0025@e.ntu.edu.sg>
2019-10-06 08:19:34 +0800

    Add util function to get contracts instances
    Signed-off-by: Fransisca Andriani <fran0025@e.ntu.edu.sg>

---
+++ client/src/utils/getContracts.js
@@ -0,0 +1,40 @@
+import AuctionFactory from "../contracts/AuctionFactory.json";
+import Auction from "../contracts/Auction.json";
+import Pedersen from "../contracts/Pedersen.json";
+
+export async function getAuctionFactoryContract(web3) {
+  try {
+    const networkId = await web3.eth.net.getId();
+    const deployedNetwork = AuctionFactory.networks[networkId];
+    const instance = new web3.eth.Contract(
+      AuctionFactory.abi,
+      deployedNetwork && deployedNetwork.address
+    );
+    return instance;
+  } catch (error) {
+    alert(`Failed to load AuctionFactory contract.`);
+    console.error(error);
+  }
+}
+
+export async function getAuctionContract(web3, address) {
+  try {
+    const instance = new web3.eth.Contract(Auction.abi, address);
+    return instance;
+  } catch (error) {
+    alert(`Failed to load Auction contract.`);
+    console.error(error);
+  }
+}
+
+export async function getPedersenContract(web3) {
+  try {
+    const auctionFactory = await getAuctionFactoryContract(web3);
+    const pedersenAddress = auctionFactory.methods.pedersenAddress().call();
+    const instance = new web3.eth.Contract(Pedersen.abi, pedersenAddress);
+    return instance;
+  } catch (error) {
+    alert(`Failed to load Pedersen contract.`);
+    console.error(error);
+  }
+}

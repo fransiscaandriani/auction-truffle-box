938953f1cf89f083f476432e9d3aada96f08c087
Eko Edita <ekoeditaa@gmail.com>
2019-10-07 04:33:57 +0800

    Implement auction page
    Signed-off-by: Eko Edita <ekoeditaa@gmail.com>

--- client/src/components/AuctionPage/index.js
+++ client/src/components/AuctionPage/index.js
@@ -1,39 +1,107 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import { Container, Typography, Button, TextField } from "@material-ui/core";
+import React, { useEffect, useState, useCallback } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Container, Typography, Button, TextField } from '@material-ui/core';
+import { placeBid } from '../../service/bidderService';
+import { getLoadedWeb3 } from '../../utils/getWeb3';
+import getCurrentAccount from '../../utils/getCurrentAccount';
+import { getAuctionContract } from '../../utils/getContracts';
+import debounce from 'lodash/debounce';
 
 const useStyles = makeStyles({
   title: {
-    marginTop: 40
+    marginTop: 40,
   },
   image: {
     width: 600,
     margin: 20,
     marginLeft: 0,
     height: 300,
-    objectFit: "cover"
+    objectFit: 'cover',
   },
   description: {
-    maxWidth: 600
+    maxWidth: 600,
   },
   action: {
-    display: "flex"
+    display: 'flex',
   },
-  "button-container": {
-    display: "flex",
-    flexDirection: "column",
-    height: 50
+  'button-container': {
+    display: 'flex',
+    flexDirection: 'column',
+    height: 50,
   },
   input: {
-    margin: 10
+    margin: 10,
   },
   end: {
-    margin: 20
-  }
+    margin: 20,
+  },
 });
 
 function AuctionPage() {
   const classes = useStyles();
+  const [bid, setBid] = useState(0);
+  const [account, setAccount] = useState('');
+  const [auctionContract, setAuctionContract] = useState({});
+  const [web3, setWeb3] = useState({});
+  const [cipher, setCipher] = useState('');
+
+  useEffect(() => {
+    async function fetchData() {
+      console.log('inside fetch data');
+      try {
+        // Get network provider and web3 instance.
+        console.log('inside try');
+        const web3 = await getLoadedWeb3();
+        console.log('web3', web3);
+        setWeb3(web3);
+
+        const auctionContract = await getAuctionContract(
+          web3,
+          '0x5e19053f26BC45c38C6e5612aDf6025b5e3D768f'
+        );
+        setAuctionContract(auctionContract);
+        console.log('auction cotnract: ', auctionContract);
+
+        const account = await getCurrentAccount(web3);
+        console.log('account: ', account);
+        setAccount(account);
+      } catch (error) {
+        // Catch any errors for any of the above operations.
+        alert(
+          `Failed to load web3, accounts, or contract. Check console for details.`
+        );
+        console.error(error);
+      }
+    }
+
+    console.log('inside usestate');
+    fetchData();
+  }, []);
+
+  const debouncedSetBid = useCallback(
+    debounce(value => setBid(value), 700),
+    []
+  );
+
+  const placeNewBid = async () => {
+    const cipher = await placeBid(account, web3, auctionContract, bid);
+    setCipher(cipher);
+  };
+
+  console.log('account:', account);
+
+  console.log('bid', bid);
+
+  const renderCipher = () => {
+    console.log('cipher', cipher);
+    if (cipher !== '') {
+      return (
+        <Typography className={classes.end} variant="h5">
+          Cipher: {cipher}
+        </Typography>
+      );
+    } else return null;
+  };
 
   return (
     <Container maxWidth="md">
@@ -46,24 +114,27 @@ function AuctionPage() {
           src="https://pbs.twimg.com/media/DVHZv6cWsAIr4at.jpg:large"
           alt="auction-item"
         />
-        <div className={classes["button-container"]}>
+        <div className={classes['button-container']}>
           <Typography className={classes.end} variant="h5">
             Auction closes in 12:12:22
           </Typography>
+          {renderCipher()}
           <TextField
             id="outlined-number"
             label="Your bid"
             type="number"
             className={classes.textField}
             InputLabelProps={{
-              shrink: true
+              shrink: true,
             }}
+            defaultValue={1}
             margin="normal"
             variant="outlined"
+            onChange={e => debouncedSetBid(e.target.value)}
+            required={true}
           />
-          <Button variant="contained" color="primary">
-            {" "}
-            Submit Bid{" "}
+          <Button variant="contained" color="primary" onClick={placeNewBid}>
+            Submit Bid
           </Button>
         </div>
       </div>

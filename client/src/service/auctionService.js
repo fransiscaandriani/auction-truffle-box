5f006224a8d0628132afd4111a24ffc161d69acf
Fransisca Andriani <fran0025@e.ntu.edu.sg>
2019-10-11 04:46:43 +0800

    Add try catch to create auction
    Signed-off-by: Fransisca Andriani <fran0025@e.ntu.edu.sg>

--- client/src/service/auctionService.js
+++ client/src/service/auctionService.js
@@ -25,31 +25,36 @@ export async function createAuction(
   const rsaKey = Cryptico.generateRSAKey(auctionData.passphrase, 1024);
   const publicKey = Cryptico.publicKeyString(rsaKey);
   let result;
-  async function sendData() {
-    await contract.methods
-      .createAuction(
-        auctionData.name,
-        auctionData.desc,
-        auctionData.bidEndTime,
-        auctionData.revealTime,
-        auctionData.winnerPaymentTime,
-        auctionData.maxBiddersCount,
-        auctionData.fairnessFees,
-        publicKey,
-        10,
-        auctionData.testing
-      )
-      .send({
-        from: account,
-        value: web3.utils.toWei(auctionData.fairnessFees.toString(), "ether")
-      })
-      .then(function(receipt) {
-        console.log(receipt);
-        result = receipt.events.AuctionCreated.returnValues.auctionContract;
-      });
+  try {
+    async function sendData() {
+      await contract.methods
+        .createAuction(
+          auctionData.name,
+          auctionData.desc,
+          auctionData.bidEndTime,
+          auctionData.revealTime,
+          auctionData.winnerPaymentTime,
+          auctionData.maxBiddersCount,
+          auctionData.fairnessFees,
+          publicKey,
+          10,
+          auctionData.testing
+        )
+        .send({
+          from: account,
+          value: web3.utils.toWei(auctionData.fairnessFees.toString(), "ether")
+        })
+        .then(function(receipt) {
+          console.log(receipt);
+          result = receipt.events.AuctionCreated.returnValues.auctionContract;
+        });
+    }
+    await sendData();
+    return result;
+  } catch (error) {
+    alert(`Transaction unsuccessful`);
+    return null;
   }
-  await sendData();
-  return result;
 }
 
 export async function getAllAuctionsData(contract) {

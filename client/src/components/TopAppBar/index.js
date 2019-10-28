dba02051c8972be19e4881fa3e0f35fbfc02c040
Eko Edita Limanta <ekoeditaa@gmail.com>
2019-10-28 21:50:54 +0800

    Add links to top bar
    Signed-off-by: Eko Edita Limanta <ekoeditaa@gmail.com>

--- client/src/components/TopAppBar/index.js
+++ client/src/components/TopAppBar/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
+import { Link } from "react-router-dom";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
@@ -13,7 +14,15 @@ const useStyles = makeStyles(theme => ({
     marginRight: theme.spacing(2)
   },
   title: {
-    flexGrow: 1
+    flexGrow: 1,
+    color: "white",
+    textDecoration: "none"
+  },
+  link: {
+    textDecoration: "none"
+  },
+  button: {
+    color: "white"
   }
 }));
 
@@ -24,10 +33,12 @@ export default function ButtonAppBar() {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-            Blind Auction
-          </Typography>
-          <Button color="inherit">My Bids</Button>
+          <Link to="/auctions" className={classes.title}>
+            <Typography variant="h6">Blind Auction</Typography>
+          </Link>
+          <Link to="/my-auctions" className={classes.link}>
+            <Button className={classes.button}>My Bids</Button>
+          </Link>
         </Toolbar>
       </AppBar>
     </div>

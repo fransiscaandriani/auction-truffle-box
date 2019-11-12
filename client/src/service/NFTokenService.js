1313b61af4429f5be5b43da2e61c7e0a0beb45e3
Fransisca Andriani <fran0025@e.ntu.edu.sg>
2019-11-12 21:46:39 +0800

    Add method for NFToken
    Signed-off-by: Fransisca Andriani <fran0025@e.ntu.edu.sg>

---
+++ client/src/service/NFTokenService.js
@@ -0,0 +1,13 @@
+export async function mintNFToken(contract, account, uri) {
+  const randomHex = `0x${Math.floor(Math.random() * 10e14)
+    .toString(16)
+    .padEnd(64, "0")}`; // create a random number, left-padded to 64 octets
+
+  try {
+    await contract.methods.mint(randomHex, uri).send({ from: account });
+    console.log(randomHex);
+    return randomHex;
+  } catch (error) {
+    console.log(error);
+  }
+}

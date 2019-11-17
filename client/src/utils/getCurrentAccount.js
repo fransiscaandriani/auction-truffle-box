cf7bd07326e7fc3cbe4db0a61ffe15fb7131d7d2
Fransisca Andriani <fran0025@e.ntu.edu.sg>
2019-11-17 20:19:18 +0800

    add function to ob tain pub key
    Signed-off-by: Fransisca Andriani <fran0025@e.ntu.edu.sg>

--- client/src/utils/getCurrentAccount.js
+++ client/src/utils/getCurrentAccount.js
@@ -1,6 +1,21 @@
+import accountList from "./accounts";
+// import Wallet from "ethereumjs-wallet";
+var Wallet = require("ethereumjs-wallet");
+var EthUtil = require("ethereumjs-util");
+
 export async function getCurrentAccount(web3) {
   const accounts = await web3.eth.getAccounts();
   return accounts[0];
 }
 
+export async function getPublicKey(account) {
+  console.log(accountList);
+  const privateKey = "0x" + accountList[account];
+  console.log("1", privateKey);
+  const privateKeyBuffer = await EthUtil.toBuffer(privateKey);
+  console.log("2", privateKeyBuffer);
+  const wallet = Wallet.fromPrivateKey(privateKeyBuffer);
+  const publicKey = wallet.getPublicKeyString();
+  return publicKey;
+}
 export default getCurrentAccount;
